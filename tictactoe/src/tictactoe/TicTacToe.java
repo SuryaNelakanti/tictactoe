83daa7a56c237109a81eacb5aa81a06991964951
Suryateja Nelakanti <47491728+SuryaNelakanti@users.noreply.github.com>
2019-08-04 19:45:09 +0530

    Add files via upload

--- tictactoe/src/tictactoe/TicTacToe.java
+++ tictactoe/src/tictactoe/TicTacToe.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class TicTacToe {
 	public static void main(String args[]) {
-		System.out.println("The locations are:");
+		System.out.println("The locations are:");														//Location of the tictactoe table.
 		boardloc();
 	}
 	
@@ -11,18 +11,18 @@ public class TicTacToe {
 		int loc,i=0,j=0,cnt=1;
 		char orx;
 		Scanner scan = new Scanner(System.in);
-		for(i=0;i<5;i++) {
-			if(i==1||i==3) {
+		for(i=0;i<5;i++) {																				//5 rows table			
+			if(i==1||i==3) {																			//for the horizontal lines
 				System.out.println("-----");
 				continue;
 			}
 			for(j=0;j<3;j++) {
-				if(j!=2) {
+				if(j!=2) {																				//the leftmost and the center character in table	
 					System.out.print(cnt+"|");
 				}
 				else
 				{
-					System.out.print(cnt+"\n");
+					System.out.print(cnt+"\n");															//the third element in a table			
 				}
 				cnt++;
 			}
@@ -32,19 +32,35 @@ public class TicTacToe {
 		System.out.println("X or O:");
 		orx = scan.next().charAt(0);
 		locfig(loc,orx);
+		scan.close();
 	}
 	
 	public static void locfig(int x, char y) {
-		char[][] board;
-		board = new char[3][3];
+		int[][] board;
+		char pos = 'x';																					//not entirely sure if creating of these characters were necessary
+		char pas = 'X';
+		char los = 'y';
+		char las = 'Y';
+		int dec=0;
+		if(y==pos||y==pas) {																			//converted the x and o to 1 and 0 so that it is easier to code the array logic
+			dec = 1;																								
+		}
+		else if(y==los||y==las) {
+			dec = 0;
+		}
+		else {
+			System.out.println("Enter either X or O:");													//incase an invalid character is entered, it will loop back to the input function
+			boardloc();
+		}
+		board = new int[3][3];
 		if(x<4) {
-			board[0][x-1]=y;
+			board[0][x-1]=dec;
 		}
 		else if(x<7 && x>3) {
-			board[1][x-3]=y;
+			board[1][x-3]=dec;
 		}
 		else if(x>7&&x<10){
-			board[2][x-7]=y;
+			board[2][x-7]=dec;
 		}
 		else {
 			System.out.println("Invalid Location\n");
@@ -53,7 +69,7 @@ public class TicTacToe {
 		dboard(board);
 	}
 	
-	public static void dboard(char[][] brd){
+	public static void dboard(int[][] brd){
 		int i=0,j=0,k=0;
 		for(i=0;i<5;i++) {
 			if(i==1||i==3) {
@@ -62,17 +78,33 @@ public class TicTacToe {
 			}
 			for(k=0;k<3;k++) {
 				for(j=0;j<3;j++) {
-/*					if(j!=2) {
-						System.out.print(brd[k][j]+"|");
+					if(j!=2) {
+						if(brd[k][j]==1) {																
+							System.out.print("X"+"|");
+						}
+						else if(brd[k][j]==0){
+							System.out.print("O"+"|");													//had to use a lot of loops for making the output table		
+						}
+						else {
+							System.out.print(" "+"|");
+						}
 					}
 					else
 					{
-						System.out.print(brd[k][j]+"\n");
-					} */
-					System.out.println(brd[k][j]);
+						if(brd[k][j]==1) {
+							System.out.print("X"+"\n");
+						}
+						else if(brd[k][j]==0){
+							System.out.print("O"+"\n");
+						}
+						else {
+							System.out.print(" "+"\n");
+						}
+					}
 				}
 			}	
 		}
+		
 		
 	}
 }
